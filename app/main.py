9d5f7ee02a4e26b9cef814d86e6d956a8384cdcb
MihailR <MihailR.20@yandex.ru>
2023-12-08 12:01:22 +0500

    fix text names

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 from app.routes.count import router as count_router
 from app.routes.posts import router as posts_comments_router
-from app.routes.subbredits import router as subreddit_router
+from app.routes.subreddits import router as subreddit_router
 
 app = FastAPI(
     title="Парсер Reddit",
